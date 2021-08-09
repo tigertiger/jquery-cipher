e394c2441f64e7e903b3573338199656a37a7eeb
Melissa <melissa.scmi@gmail.com>
2021-08-09 15:32:05 -0700

    add a million functions
    Co-authored-by: Erica Marroquin <ericamarroquin03@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,29 @@
-jQuery(document).ready(function() {
-jQuery("h1").click(function() {
-  alert("This is a heading.");
-});
-
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
-
-jQuery("img").click(function() {
-  alert("This is an image.");
-});
 
 const sentence = prompt("Please enter a sentence.");
-console.log(sentence);
 
-function firstLastLetter () {
-const firstLetter = sentence.charAt(0);
-const lastLetter = sentence.charAt(sentence.length - 1);
-return lastLetter.toUpperCase() + firstLetter.toUpperCase();
+function firstLastLetter (sentence) {
+  const firstLetter = sentence.charAt(0);
+  const lastLetter = sentence.charAt(sentence.length - 1);
+  return firstLetter.toUpperCase() + lastLetter.toUpperCase();
 }
-console.log(firstLastLetter());
 
-});
+function reverseFirstLastLetter (sentence) {
+  // const firstLetter = sentence.charAt(0);
+  // const lastLetter = sentence.charAt(sentence.length - 1);
+  // return lastLetter.toUpperCase() + firstLetter.toUpperCase();
+  return sentence.charAt(sentence.length - 1).toUpperCase() + sentence.charAt(0).toUpperCase();
+}
+
+function printLetters() {
+  return sentence + reverseFirstLastLetter(firstLastLetter(sentence));
+}
+
+function countLetters() {
+  const roundy = Math.floor(sentence.length / 2);
+  console.log(sentence.length);
+  console.log(roundy);
+  return sentence.charAt(roundy);
+}
+
+console.log(printLetters());
+console.log(countLetters());
